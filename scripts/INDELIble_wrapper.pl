6c2be83a16ff6e02ba60a62b59c12f7be1b5c9d2
Diego Mallo <dmallo@uvigo.es>
2015-01-15 13:45:26 +0100

    Rearrangements and improvements on accompanying scripts/files

--- scripts/INDELIble_wrapper.pl
+++ scripts/INDELIble_wrapper.pl
@@ -1,15 +1,21 @@
-#!/usr/bin/perl -w
+#!/opt/local/bin/perl -w
 
 # Quick perl script to simulate sequence evolution using INDELible along gene trees simulated by SimPhy.
 # It doesn't support input indelible [BRANCHES] [TREE] [EVOLVE] and just one [MODEL]
 # Diego Mallo 2015
 
 use strict;
+#GSL
+use Math::GSL::RNG qw/:all/;
+use Math::GSL::Randist qw/:all/;
+use Math::GSL::CDF qw/:all/;
+my $rng = Math::GSL::RNG->new();
 
 #Config
 my $w_dir;
 my $input_file;
-my $length;
+my $my_length;
+my $length_command;
 my $n_threads=0; #Maximum by default
 
 my @files;
@@ -31,12 +37,85 @@ my $n_digits=0;
 my $trees;
 my $itree;
 
+sub parse_sampling
+{
+	my $sampling_string="";
+	my $distribution_code="";
+	my $is_integer="";
+	my $ret_value=0;
+	
+	($sampling_string)=@_;
+	$sampling_string=~m/^(.*?):/;
+	$distribution_code=uc($1);
+	if ($sampling_string=~/:i$/i)
+	{
+		$is_integer=1;
+	}
+	else
+	{
+		$is_integer=0;
+	}
+	my @params=();
+	while ($sampling_string=~m/([+-]?((\d+(\.\d*)?)|(\.\d+)))/g)
+	{
+		push(@params,$1);
+	}
+	if ($distribution_code eq "F")
+	{
+		scalar @params == 1 or die "Incorrect number of parameters for a fixed value. Sampling command \"$sampling_string\"";
+		$ret_value=$params[0];
+	}
+	elsif ($distribution_code eq "N")
+	{
+		scalar @params == 2 or die "Incorrect number of parameters sampling a normal distribution. Sampling command \"$sampling_string\"";
+		$ret_value=gsl_ran_gaussian_ziggurat($rng->raw(),@params);
+	}
+	elsif ($distribution_code eq "L")
+	{
+		scalar @params == 2 or die "Incorrect number of parameters sampling a lognormal distribution. Sampling command \"$sampling_string\"";
+		$ret_value=gsl_ran_lognormal($rng->raw(),@params);
+	}
+	elsif ($distribution_code eq "U")
+	{
+		scalar @params == 2 or die "Incorrect number of parameters sampling an uniform distribution. Sampling command \"$sampling_string\"";
+		$ret_value=gsl_ran_flat($rng->raw(),@params);
+	}
+	elsif ($distribution_code eq "E")
+	{
+		scalar @params == 1 or die "Incorrect number of parameters sampling an exponential distribution. Sampling command \"$sampling_string\"";
+		$ret_value=gsl_ran_exponential($rng->raw(),@params);
+	}
+	elsif ($distribution_code eq "G")
+	{
+		scalar @params == 2 or die "Incorrect number of parameters sampling a gamma distribution. Sampling command \"$sampling_string\"";
+		$ret_value=gsl_ran_gamma($rng->raw(),@params);
+	}
+	elsif ($distribution_code eq "SL")
+	{
+		scalar @params == 3 or die "Incorrect number of parameters sampling a lognormal distribution multiplied by a constant. Sampling command \"$sampling_string\"";
+		$ret_value=gsl_ran_lognormal($rng->raw(),$params[0],$params[1])*$params[2];
+	}
+	else
+	{
+		die "Unknown distribution code \"$distribution_code\" present in the sampling command \"$sampling_string\"";
+	}
+	
+	if ($is_integer==1)
+	{
+		return int $ret_value;
+	}
+	else
+	{
+		return $ret_value;
+	}
+}
+
 if ($#ARGV != 3)
 {
 	die "Incorrect number of parameters, Usage: script.pl directory input_config length numberofcores\n";
 }
 
-($w_dir,$input_file, $length,$n_threads)=@ARGV;
+($w_dir,$input_file, $length_command,$n_threads)=@ARGV;
 chdir($w_dir) or die "Error changing the working dir to $w_dir\n";
 $w_dir=~m/([^\/]*).?$/;
 $w_dir=$1;
@@ -129,9 +208,11 @@ foreach my  $dir (@dirs)
 		$temp_model=$models[0];
 		$temp_model=~s/(\[MODEL\])\s*(.*?)\s*\n/$1 $2$id\n/;
 		$model_id="$2$id";
+		$temp_model=~s/\$\((.*?)\)/parse_sampling($1)/ge;
 		$out_models.="$temp_model\n";
 		$trees.=sprintf("\[TREE\] T%.*d %s\n",$n_digits,$locus,$itree);
-		$partitions.=sprintf("\[PARTITIONS\] T%.*d \[T%.*d %s %s\]\n",$n_digits,$locus,$n_digits,$locus,$model_id,$length);
+		$my_length=parse_sampling($length_command);
+		$partitions.=sprintf("\[PARTITIONS\] T%.*d \[T%.*d %s %s\]\n",$n_digits,$locus,$n_digits,$locus,$model_id,$my_length);
 		$evolves.=sprintf("T%.*d 1 %.*d\n",$n_digits,$locus,$n_digits,$locus);
 		
 		$sequence_counter+=1;
